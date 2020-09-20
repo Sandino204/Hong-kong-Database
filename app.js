610ce5e45b646a457339783f90804bafc72ab108
Sandino204 <caiogustavoxaviermelo@gmail.com>
2020-09-20 15:19:15 -0300

    Express sessions part 2

--- app.js
+++ app.js
@@ -43,45 +43,28 @@ app.use(session({
   store: new fileStore()
 }))
 
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next){
   console.log(req.session)
 
   if(!req.session.user){
-    var authHeader = req.headers.authorization
+  
+    var err = new Error('You Are not autenticated!')
 
-    if(!authHeader){
-      var err = new Error('You Are not autenticated!')
+    res.setHeader('WWW-Authenticate', 'basic')
+    err.status = 403
 
-      res.setHeader('WWW-Authenticate', 'basic')
-      err.status = 401
-
-      return next(err)
-    }
-
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
-
-    var username = auth[0]
-    var password = auth[1]
-
-    if (username === 'admin'  && password === 'password'){
-      req.session.user = 'admin'
-      next()
-    }else{
-      var err = new Error('You Are not autenticated!')
-
-      res.setHeader('WWW-Authenticate', 'basic')
-      err.status = 401
-
-      return next(err)
-    }
-
+    return next(err)
   }else{
-    if(req.session.user === 'admin'){
+    if(req.session.user === 'authenticated'){
       next()
     }else{
       var err = new Error('You Are not autenticated!')
 
-      err.status = 401
+      err.status = 403
 
       return next(err)
     }
@@ -93,8 +76,6 @@ app.use(auth)
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter)
 app.use('/promotions', promoRouter)
 app.use('/leaders', leaderRouter)
