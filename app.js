cdf3c4a722abb9e8691c5db948f26bd094086255
Sandino204 <caiogustavoxaviermelo@gmail.com>
2020-10-10 05:53:36 -0300

    Passport

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var session = require('express-session')
 var fileStore = require('session-file-store')(session)
+var passport = require('passport')
+var authenticate = require('./authenticate')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -43,31 +45,26 @@ app.use(session({
   store: new fileStore()
 }))
 
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next){
-  console.log(req.session)
 
-  if(!req.session.user){
-  
+  if(!req.user){
+
     var err = new Error('You Are not autenticated!')
-
-    res.setHeader('WWW-Authenticate', 'basic')
     err.status = 403
-
     return next(err)
+
   }else{
-    if(req.session.user === 'authenticated'){
-      next()
-    }else{
-      var err = new Error('You Are not autenticated!')
 
-      err.status = 403
+    next()
 
-      return next(err)
-    }
   }
 
 }
